a2538f2d6e35e0ae47d753db305e43a5e7a9e3a5
Louis Nguyen <sycuown@gmail.com>
2025-02-14 20:43:20 -0800

    backend setup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -11,7 +11,9 @@ export const signup = async (req, res) => {
     }
 
     if (password.length < 6) {
-      return res.status(400).json({ message: "Password must be at least 6 characters" });
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
     }
 
     const user = await User.findOne({ email });
@@ -28,7 +30,7 @@ export const signup = async (req, res) => {
     });
 
     if (newUser) {
-      // generate jwt token here
+      // generate jwt token
       generateToken(newUser._id, res);
       await newUser.save();
 
